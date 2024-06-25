98e6f22f28b350db30b2d6350586ef1cddcdc21b
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-06-25 23:10:47 +0530

    feat:added commands

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { LoginDTO, RegisterDTO } from './auth.dto';
 import { UserHelper } from 'src/helper/user.helper';
 import { AuthService } from './auth.service';
-
+//
 @Controller('auth')
 export class AuthController {
   constructor(
